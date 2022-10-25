9f0b77e315a902698e207ec1be78fecea9d78f29
Fainradius <evgyeniychernov@mail.ru>
2022-10-25 22:23:40 +0300

    Первое задание

---
+++ OneDrive/Рабочий стол/Homework C#/Homework001/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число 1");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число 2");
+int b = int.Parse(Console.ReadLine()!);
+if(a>b)
+{
+ Console.WriteLine("a больше b");
+}
+else
+{
+ Console.WriteLine("b больше a");   
+}
